05390b4e885564196b82de334ed9d1beedf07457
Will <wdportman@gmail.com>
2020-12-29 19:55:08 -0800

    Complete day 4B

--- day4B.js
+++ day4B.js
@@ -37,23 +37,79 @@ fs.readFile('day4data.txt', 'utf8', (err, data) => {
         obj[strParts[0]] = strParts[1];
       }
       return obj;
-    }, {});    
+    }, {});
     formattedDataArray.push(newRow);
   };
 
   // Check that the data present in each passport is also valid:
   for (const row of formattedDataArray) {
+    let validDataCounter = 0;
+    
+    // byr
+    if (row.byr >= 1920 && row.byr <= 2002) {
+      validDataCounter += 1;
+    };
 
+    // iyr
+    if (row.iyr >= 2010 && row.iyr <= 2020) {
+      validDataCounter += 1;
+    };
+
+    // eyr
+    if (row.eyr >= 2020 && row.eyr <= 2030) {
+      validDataCounter += 1;
+    };
+
+    // hgt
+    let hgtcm = row.hgt.slice(0,3);
+    let hgtin = row.hgt.slice(0,2);
+    if (row.hgt.includes('cm') && !row.hgt.includes('in') && hgtcm >= 150 && hgtcm <= 193) {
+      validDataCounter += 1;
+    };
+    if (!row.hgt.includes('cm') && row.hgt.includes('in') && hgtin >= 59 && hgtin <= 76) {
+      validDataCounter += 1;
+    };
+
+    // hcl
+    const hclValidChars = '#abcdef0123456789';
+    let invalidHcl = 0;
+    for (const character of row.hcl) {
+      if (!hclValidChars.includes(character)) {
+        invalidHcl += 1;
+      }
+    }
+    if (row.hcl[0] !== '#') {
+      invalidHcl += 1;
+    };
+    if (invalidHcl === 0) {
+      validDataCounter += 1;
+    };
+
+    // ecl
+    const eyeColors = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'];
+    if (eyeColors.includes(row.ecl)) {
+      validDataCounter += 1;
+    };
+
+    // pid
+    const pidValidChars = '0123456789';
+    let invalidPid = 0;
+    for (const character of row.pid) {
+      if (!pidValidChars.includes(character)) {
+        invalidPid += 1;
+      }
+    };
+    if (row.pid.length !== 9) {
+      invalidPid += 1;
+    };
+    if (invalidPid === 0) {
+      validDataCounter += 1;
+    };
+
+    // total valid:
+    if (validDataCounter === terms.length) {
+      validCounter += 1;
+    }
   }
-  console.log("formattedDataArray:", formattedDataArray)
+  console.log("validCounter:", validCounter)
 });
-
-// let foo = data.split("\n").reduce(function(obj, str, index) {
-//   let strParts = str.split(":");
-//   if (strParts[0] && strParts[1]) { //<-- Make sure the key & value are not undefined
-//     obj[strParts[0].replace(/\s+/g, '')] = strParts[1].trim(); //<-- Get rid of extra spaces at beginning of value strings
-//   }
-//   return obj;
-// }, {});
-
-// console.log(foo);
