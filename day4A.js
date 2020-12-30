014ce15bfafa1fe0c022a031e6cd5999a05fb409
Will <wdportman@gmail.com>
2020-12-29 17:18:12 -0800

    Solve Day 4A

--- day4A.js
+++ day4A.js
@@ -1,16 +1,23 @@
 const fs = require('fs');
 
-const terms = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid', 'cid']
-
+const terms = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
 
 fs.readFile('day4data.txt', 'utf8', (err, data) => {
+
   const dataArray = data.split('\n\n');
+
   let validCounter = 0;
+
   for (const row of dataArray) {
-    if (row.includes('byr')) {
+    let includesCounter = 0;
+    for (const term of terms) {
+      if (row.includes(term)) {
+        includesCounter += 1;
+      }
+    }
+    if (includesCounter === terms.length) {
       validCounter += 1;
     }
   }
   console.log("validCounter:", validCounter)
-  console.log("dataArray length:", dataArray.length)
 })
