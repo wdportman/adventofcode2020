cdfe92659d10da2ee0c219abe7ace47c790843ae
Will <wdportman@gmail.com>
2020-12-30 20:12:22 -0800

    Solve day6A using map & reduce functions

--- day6A.js
+++ day6A.js
@@ -1,6 +1,37 @@
 const fs = require('fs');
+const { runInNewContext } = require('vm');
 
-fs.readFile('week1/day6data.txt', 'utf8', (err, data) => {
+fs.readFile('day6data.txt', 'utf8', (err, data) => {
+
+  // Format data so each group is one string:
   const dataArray = data.split('\n\n');
-  console.log(dataArray);
+  const formattedArray = [];
+  for (const row of dataArray) {
+    const rowArray = row.split('\n');
+    const newRow = rowArray.join('');
+    formattedArray.push(newRow);
+  }
+
+  // Get the # of unique questions per group:
+  const individualSums = formattedArray.map(function(group) {
+
+    let usedCharacters = '';
+    let uniqueCharacterCounter = 0;
+    
+    for (const character of group) {
+      if (!usedCharacters.includes(character)) {
+        usedCharacters += character;
+        uniqueCharacterCounter += 1;
+      }
+    }
+    return uniqueCharacterCounter;
+  })
+
+  // Sum all groups' # of unique questions:
+  const total = individualSums.reduce(function(tally, current) {
+    return tally + current
+  }, 0);
+
+  console.log(total);
+
 })
